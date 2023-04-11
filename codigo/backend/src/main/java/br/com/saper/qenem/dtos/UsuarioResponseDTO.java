af253498f5db8bf050503d5a2821164f08d0b24d
Nicolas David <nicolasdavid193@gmail.com>
2023-04-11 20:55:38 -0300

    roles usuário e script para alimentação do banco

--- codigo/backend/src/main/java/br/com/saper/qenem/dtos/UsuarioResponseDTO.java
+++ codigo/backend/src/main/java/br/com/saper/qenem/dtos/UsuarioResponseDTO.java
@@ -1,10 +1,13 @@
 package br.com.saper.qenem.dtos;
 
+import br.com.saper.qenem.models.Role;
 import br.com.saper.qenem.models.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,11 +23,14 @@ public class UsuarioResponseDTO {
 
     private String email;
 
+    private List<Role> roles;
+
     public UsuarioResponseDTO(Usuario usuario) {
         this.id = usuario.getId();
         this.nome = usuario.getNome();
         this.login = usuario.getLogin();
         this.email = usuario.getEmail();
+        this.roles = usuario.getRoles();
     }
 
 }
