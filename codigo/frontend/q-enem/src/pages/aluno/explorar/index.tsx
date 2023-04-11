fa03c17a04fab515aa46ab721ca383e635f2cdba
Nicolas David <nicolasdavid193@gmail.com>
2023-04-11 19:46:11 -0300

    NavbarAluno finalizada

--- codigo/frontend/q-enem/src/pages/aluno/explorar/index.tsx
+++ codigo/frontend/q-enem/src/pages/aluno/explorar/index.tsx
@@ -1,5 +1,90 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { BsArrowLeft as Voltar } from 'react-icons/bs'
+import Container from 'react-bootstrap/Container'
 
 export default function AlunoExplorar() {
-  return <div>Explorar</div>
+  const navigate = useNavigate()
+  const [filtro, setFiltro] = useState('Padrão')
+  const [filtroDisciplina, setFiltroDisciplina] = useState('Tudo')
+
+  return (
+    <Container>
+      <main className='d-flex flex-column justify-content-center'>
+        <div className='btn-group mb-3 w-75' role='group' aria-label='filtro-disciplinas'>
+          <input
+            type='radio'
+            className='btn-check'
+            name='filtro-disciplinas'
+            id='btnTudo'
+            autoComplete='off'
+            checked
+          />
+          <label className='btn btn-outline-primary' htmlFor='btnTudo'>
+            TUDO
+          </label>
+
+          <input
+            type='radio'
+            className='btn-check'
+            name='filtro-disciplinas'
+            id='btnLinguagens'
+            autoComplete='off'
+          />
+          <label className='btn btn-outline-primary' htmlFor='btnLinguagens'>
+            LINGUAGENS
+          </label>
+
+          <input
+            type='radio'
+            className='btn-check'
+            name='filtro-disciplinas'
+            id='btnHumanas'
+            autoComplete='off'
+          />
+          <label className='btn btn-outline-primary' htmlFor='btnHumanas'>
+            HUMANAS
+          </label>
+
+          <input
+            type='radio'
+            className='btn-check'
+            name='filtro-disciplinas'
+            id='btnNatureza'
+            autoComplete='off'
+          />
+          <label className='btn btn-outline-primary' htmlFor='btnNatureza'>
+            NATUREZA
+          </label>
+
+          <input
+            type='radio'
+            className='btn-check'
+            name='filtro-disciplinas'
+            id='btnMatematica'
+            autoComplete='off'
+          />
+          <label className='btn btn-outline-primary' htmlFor='btnMatematica'>
+            MATEMÁTICA
+          </label>
+        </div>
+
+        <div className='border border-primary w-75 bg-light text-uppercase'>
+          <div className='fs-6 border-bottom border-primary text-truncate p-2'>
+            <a href='' className='text-decoration-none link-dark'>
+              [linguagens 3045] - Seu Osvaldo foi passear na rua com seu cachorro e sua esposa,
+              parou pra comer um cachorro quente e um churros de doce de leite
+            </a>
+          </div>
+
+          <div className='fs-6 border-bottom border-primary text-truncate p-2'>
+            <a href='' className='text-decoration-none link-dark'>
+              [linguagens 3045] - Seu Osvaldo foi passear na rua com seu cachorro e sua esposa,
+              parou pra comer um cachorro quente e um churros de doce de leite
+            </a>
+          </div>
+        </div>
+      </main>
+    </Container>
+  )
 }
