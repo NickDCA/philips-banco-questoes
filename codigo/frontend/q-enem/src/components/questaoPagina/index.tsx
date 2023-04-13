40c4dabbefb4d3c14e7e9e2256dd231b1c1eb14e
Nicolas David <nicolasdavid193@gmail.com>
2023-04-13 16:39:25 -0300

    Feature Explorar + Questão Única - DONE

--- codigo/frontend/q-enem/src/components/questaoPagina/index.tsx
+++ codigo/frontend/q-enem/src/components/questaoPagina/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useContext, useEffect } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useAPI } from 'services/API'
 import AuthContext from 'store/authContext'
-import QuestoesContext, { ItemQuestao, Questao } from 'store/questoesContext'
+import { Questao } from 'store/questoesContext'
 import Form from 'react-bootstrap/Form'
-import { BsCheck } from 'react-icons/bs'
+import { BsCheck, BsFillCheckCircleFill, BsXCircleFill } from 'react-icons/bs'
 // export type Questao = {
 //     id: number
 //     materia: string
@@ -19,7 +19,6 @@ export default function QuestaoPagina() {
   const api = useAPI()
   const auth = useContext(AuthContext)
   const parametros = useParams()
-  const idQuestao = parametros
   const htmlConfig = {
     headers: {
       'Content-Type': 'application/json',
@@ -30,6 +29,7 @@ export default function QuestaoPagina() {
 
   const [questao, setQuestao] = useState<Questao>()
   const [score, setScore] = useState<boolean>(false)
+  const [showResult, setShowResult] = useState<boolean>(false)
   //   const itensQuestao = questao?.itensQuestoes
   //   const questaoId = String(questao?.id)
   //   const itensId = questao?.itensQuestoes.map((item) => String(item.id))
@@ -37,41 +37,65 @@ export default function QuestaoPagina() {
   useEffect(() => {
     api.get(`questoes/${parametros.id}`, {}, htmlConfig).then((resposta) => setQuestao(resposta))
     console.log(questao)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const handleSubmit = (e: any) => {
     e.preventDefault()
-    score ? console.log('Acertou!') : console.log('Errou!')
+    setShowResult(true)
   }
   return (
     <div className='card mt-3'>
-      <div className='card-header'>{questao?.enunciado}</div>
+      <div className='card-header'>
+        {' '}
+        <h2>
+          [{questao?.materia} #{questao?.id}]
+        </h2>{' '}
+        <p className='lead'>{questao?.enunciado}</p>{' '}
+      </div>
       <div className='card-body'>
         <Form onSubmit={(e) => handleSubmit(e)}>
           <div key={questao?.id}>
             {questao?.itensQuestao?.map((item) => {
               return (
                 <Form.Check
+                  disabled={showResult}
                   type='radio'
                   id={String(item.id)}
                   name={`questao ${questao?.id}`}
                   label={item.texto}
-                  onChange={(e) => (item.correto ? setScore(true) : setScore(false))}
+                  onChange={() => (item.correto ? setScore(true) : setScore(false))}
                 />
               )
             })}
           </div>
 
-          <button
-            className='btn btn-outline-success mt-3'
-            type='submit'
-            data-bs-toggle='modal'
-            data-bs-target='#exampleModal'
-          >
+          <button className='btn btn-outline-success mt-3' type='submit'>
             Responder <BsCheck />
           </button>
         </Form>
       </div>
+      {showResult ? (
+        score ? (
+          <div className='card-footer bg-success-subtle text-success'>
+            <p>
+              Parabéns, você acertou! <BsFillCheckCircleFill />
+            </p>
+            <button className='btn btn-outline-secondary' onClick={() => setShowResult(false)}>
+              Tentar novamente
+            </button>
+          </div>
+        ) : (
+          <div className='card-footer bg-danger-subtle text-danger'>
+            <p>
+              Você errou, tente novamente... <BsXCircleFill />
+            </p>
+            <button className='btn btn-outline-secondary' onClick={() => setShowResult(false)}>
+              Tentar novamente
+            </button>
+          </div>
+        )
+      ) : null}
     </div>
   )
 }
